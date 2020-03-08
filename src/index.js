d78d6d56bf60b68c275079457e5d9fcf911b8c5e
FireGUN <yaroslavbig@ya.ru>
2020-03-08 21:54:49 +0200

    feet: all test pass

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprLen = expr.length;
+    const expToTenSym = [];
+
+    for(let count = 0; count < exprLen; count += 10){
+        expToTenSym.push(expr.slice(count, count + 10));
+    }
+    
+    const morse = [];
+
+    for(const str of expToTenSym) {
+        const strLen = str.length;
+        let morseSym = '';
+        if (str === '**********') {
+            morse.push(' ')
+            continue;
+        }
+        for (let count = 0; count < strLen; count += 2 ) {
+            symb = str.slice(count, count + 2);
+            if(symb != '00') {
+              morseSym +=  symb === '10' ? '.' : '-';
+            }
+        }   
+       morse.push(morseSym);
+    }
+    let result = '';
+
+    for(let morseSymb of morse) {
+       if(morseSymb.toString() === ' ') {
+           result += ' ';
+       } else {
+       const letter = MORSE_TABLE[morseSymb];
+      // console.log(morseSymb, letter);
+       result += letter;
+       }
+    }
+    return result;
 }
 
 module.exports = {
